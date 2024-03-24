82c7670507cfcde80221c08f9f4657e51e44da39
Rubén Manzano Santacruz <rmanzanointecrobots@gmail.com>
2024-03-24 17:16:28 +0100

    third commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import Image from "next/image";
-import Header from "../components/firstPage/header";
-import Welcome from "../components/firstPage/welcome";
-import About from "../components/firstPage/about";
-import Roles from "../components/firstPage/roles";
-import LoginSignup from "../components/firstPage/signinSignup";
+import Header from "@/components/firstPage/header";
+import Welcome from "@/components/firstPage/welcome";
+import About from "@/components/firstPage/about";
+import Roles from "@/components/firstPage/roles";
+import LoginSignup from "@/components/firstPage/signinSignup";
 
 const Home: React.FC = () => {
   return (
