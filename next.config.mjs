3b643321e23d124b276fe3d0fce13bbafe59b49a
Rubén Manzano Santacruz <rmanzanointecrobots@gmail.com>
2024-06-02 02:36:34 +0200

    s

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = {
-    env: {
-      MONGODB_URI: process.env.MONGODB_URI,
-      NEXTAUTH_URL_INTERNAL: process.env.NEXTAUTH_URL_INTERNAL,
-      NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    },
-  };
-
 export default nextConfig;
